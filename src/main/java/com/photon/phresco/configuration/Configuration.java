c6cc7da64851b22f8d3e84abbc5c16d031ca3861
ganeshkumar-s <ganeshkumar.sivalingam@photoninfotech.net>
2012-07-25 21:01:38 +0530

    [artf503117] adaption to get configuration based on configType and name

--- src/main/java/com/photon/phresco/configuration/Configuration.java
+++ src/main/java/com/photon/phresco/configuration/Configuration.java
@@ -1,3 +1,22 @@
+/*
+ * ###
+ * Archetype - phresco-html5-archetype
+ * 
+ * Copyright (C) 1999 - 2012 Photon Infotech Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ###
+ */
 package com.photon.phresco.configuration;
 
 import java.util.Properties;
@@ -7,16 +26,23 @@ public class Configuration {
 	private String name;
 	private String desc;
 	private String type;
+	private String envName;
 	private Properties properties;
-	private String appliesTo;
+	String appliesTo;
 
-	public Configuration(String name, String desc, String type, Properties properties, String configAppliesTo) {
+	
+	public Configuration(String name, String desc, String envName, String type, Properties properties, String configAppliesTo) {
 		super();
 		this.name = name;
 		this.desc = desc;
+		this.envName = envName;
 		this.type = type;
 		this.appliesTo = configAppliesTo;
 		this.properties = properties;
+	}
+	
+	public Configuration(String name, String desc, String type, Properties properties, String configAppliesTo) {
+		this (name, desc, null, type, properties, configAppliesTo);
 	}
 	
 	public Configuration(String name, String type) {
@@ -46,6 +72,14 @@ public class Configuration {
 		return type;
 	}
 
+	public String getEnvName() {
+		return envName;
+	}
+
+	public void setEnvName(String envName) {
+		this.envName = envName;
+	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
@@ -60,6 +94,10 @@ public class Configuration {
 
 	public void setAppliesTo(String configAppliesTo) {
 		this.appliesTo = configAppliesTo;
+	}
+	
+	public String getAppliesTo() {
+		return appliesTo;
 	}
 
 	@Override
