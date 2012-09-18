7939a2882f0d0cba9abbc48c2db202321aaff5ec
vivekraja-v <vivekraja.vasudevan@photoninfotech.net>
2012-09-18 12:27:29 +0530

    [artf533423]  - eshop test cases issue

---
+++ src/test/qunit/js/eshop/widgets/EshopNewProductsTest.js
@@ -0,0 +1,109 @@
+/*global require*/
+
+require([ "jquery", "./Newproducts", "./EShopAPI", "./WSConfig", "qunit" ], function($, Newproducts, EShopAPI, WSConfig, QUnit) {
+
+var equal = QUnit.equal, notEqual = QUnit.notEqual, expect = QUnit.expect, wsconfig, testWithSameLength = QUnit.test, testWithDifferentLength = QUnit.test, output1, output2;
+
+	testWithSameLength("Test New Products", function() {
+	
+		var newProduct, mainContent, topH3, api, self = this, navUL, productList, newproducts, product, innerLi, innerDiv1, innerDiv2, ahref, pricebtton, data, i, productlength;
+		
+		newProduct = new Newproducts();
+		api = new EShopAPI();
+		wsconfig = new WSConfig();
+		api.wsURL = wsconfig.WSConfigurl;
+		//api.getWsConfig();
+		newProduct.api = api;
+		
+		output1 = newProduct.renderUI();
+
+		mainContent = $('<div></div>');
+        topH3 = $('<h3>New Products</h3>');
+		navUL = $('<ul></ul>');
+		
+		api.getTopsellProducts(function(jsonObject){
+            productList = jsonObject;
+			newproducts = productList.product.length;
+            for (i = 0; i < newproducts; i++) {
+                product = jsonObject.product[i];
+                innerLi = $('<li></li>');
+                innerDiv1 = $('<div class="img"><a href="javascript:void(0);"><img src="' + api.wsURLWithoutContext + '/images/web/' + product.image + '" alt=""></a></div>');
+                innerDiv2 = $('<div class="info">');
+                ahref = $('<a class="title2" href="#">' + product.name + '</a><div class="price"><span class="special">Sell at:</span><span class="special">$' + product.sellPrice + '</span></div>');
+                pricebtton = $('<div class="pricebtn"><a class="addtocart_buttonstyle" href="#">Add to cart</a></div>');
+                data = {};
+
+                data.productId = product.id;
+                data.name = product.name;
+                data.quantity = 1;
+                data.price = product.sellPrice;
+                data.image = product.image;
+                data.totalPrice = (data.quantity * product.sellPrice);
+                innerDiv2.append(ahref); 
+                innerDiv2.append(pricebtton);                   
+                innerLi.append(innerDiv1);
+                innerLi.append(innerDiv2);
+                navUL.append(innerLi);
+            }  
+        });  
+
+		mainContent.append(topH3);     
+        mainContent.append(navUL);
+		
+		output2 = mainContent; 
+
+		equal(output1.html(), output2.html()," New Products - With same length - Test case passed"); 
+	
+	});
+	
+	/* testWithDifferentLength("New Products - With different length.", function() {
+	
+		var newProduct, mainContent, topH3, api, self = this, navUL, productList, newproducts, product, innerLi, innerDiv1, innerDiv2, ahref, pricebtton, data, i, productlength;
+		
+		newProduct = new Newproducts();
+		api = new EShopAPI();
+		wsconfig = new WSConfig();
+		api.wsURL = wsconfig.WSConfigurl;
+		//api.getWsConfig();
+		newProduct.api = api;
+
+		output1 = newProduct.renderUI();
+
+		productlength = 11;
+		mainContent = $('<div></div>');
+        topH3 = $('<h3>New Products</h3>');
+		navUL = $('<ul></ul>');
+		
+		api.getTopsellProducts(function(jsonObject){
+            productList = jsonObject;
+            for (i = 0; i < productlength; i++) {
+                product = jsonObject.product[i];
+                innerLi = $('<li></li>');
+                innerDiv1 = $('<div class="img"><a href="javascript:void(0);"><img src="' + api.wsURLWithoutContext + '/images/web/' + product.image + '" alt=""></a></div>');
+                innerDiv2 = $('<div class="info">');
+                ahref = $('<a class="title2" href="#">' + product.name + '</a><div class="price"><span class="special">Sell at:</span><span class="special">$' + product.sellPrice + '</span></div>');
+                pricebtton = $('<div class="pricebtn"><a class="addtocart_buttonstyle" href="#">Add to cart</a></div>');
+                
+				data = {};
+                data.productId = product.id;
+                data.name = product.name;
+                data.quantity = 1;
+                data.price = product.sellPrice;
+                data.image = product.image;
+                data.totalPrice = (data.quantity * product.sellPrice);
+                innerDiv2.append(ahref); 
+                innerDiv2.append(pricebtton);                   
+                innerLi.append(innerDiv1);
+                innerLi.append(innerDiv2);
+                navUL.append(innerLi);
+            }  
+        });  
+
+		mainContent.append(topH3);     
+        mainContent.append(navUL);
+		
+		output2 = mainContent; 
+
+		notEqual(output1.html(), output2.html(),  "New Products - With different length - Test case passed"); 
+	}); */
+});
