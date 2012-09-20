2875f79c51c07ed70b974e428cfbca385f835e90
vivekraja-v <vivekraja.vasudevan@photoninfotech.net>
2012-09-20 15:17:44 +0530

    [artf509402] - WSConfig.js file added

---
+++ src/main/js/eshop/widgets/WSConfig.js
@@ -0,0 +1,12 @@
+/*global define, $ window */
+
+define( "eshop/widgets/WSConfig", [ "jquery" ], function($) {
+	
+	function WSConfig () {
+	}
+    
+	WSConfig.prototype.WSConfigurl = "http://172.16.17.180:2020/eshop";
+	
+	return WSConfig;
+});
+
