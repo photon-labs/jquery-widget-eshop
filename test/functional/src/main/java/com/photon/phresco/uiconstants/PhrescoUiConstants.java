a800eab2eadba48fcf706a97a70b4cbb2bf25706
vairamuthu_m <vairamuthu.mottaiyan@photoninfotech.net>
2012-12-14 18:30:26 +0530

    artf576718 : Converting php technology functional testcase  into java with TestNG format

--- test/functional/src/main/java/com/photon/phresco/uiconstants/PhrescoUiConstants.java
+++ test/functional/src/main/java/com/photon/phresco/uiconstants/PhrescoUiConstants.java
@@ -10,7 +10,7 @@ public class PhrescoUiConstants {
 	public String BROWSER = "Browser";
 	public String PROTOCOL = "protocol";
 	public String PORT = "port";
-	public String RESOLUTION="resolution";
+	public String RESOLUTION="Resolution";
 
 	public PhrescoUiConstants() {
 		try {
